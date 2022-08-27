dc280fa3eb8092834ac96d2eb3ed0d78697ae946
jehopark <jeho.park@cmc.edu>
2022-08-26 17:29:11 -0700

    Update Exercise 1

--- Lab1_1.R
+++ Lab1_1.R
@@ -1,11 +1,18 @@
-# Lab1
-## Required packages
-library('ggplot2')
+# Assignment: Lab1_1
+# Student Name: 
+# Date:
+
+# Required packages
+# Load all required packages below using library function
+
 
 # Ex 3.2.4: #4 (modified)
 # Using the mpg data, make a scatterplot of hwy vs cyl
+# mpg를 이용하여 스캐터플롯(산점도)를 만드시오. x축에는 cyl를 y축에는 hwy를 둔다.
+
 ## Your code starts below this line
-p <- ggplot(data = mpg) +
-  geom_point(mapping = aes(x = hwy, y = cyl))
+
+p <- 
+
 ## Your work ends before this line
 
