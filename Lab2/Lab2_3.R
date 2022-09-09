8caf52d0d97e4154e47c717b8bbd0311ef16cdac
nunnaya <87155508+kenneth32park@users.noreply.github.com>
2022-09-09 12:29:55 -0700

    Lab 2 Added
    Updated Lab Folder Structure, Addition of Lab2_1, Lab2_2, Lab2_3

---
+++ Lab2/Lab2_3.R
@@ -0,0 +1,23 @@
+# Assignment: Lab2_3
+# Student Name: 
+# Date:
+
+# Required packages
+# Load all required packages below using the library() function.
+
+## Your libraries start below this line
+
+## Your libraries end before this line
+
+
+# Ex 5.6.7: #5 (modified)
+# On average, which carrier has the worst delays?
+# (Hint: think about flights %>% group_by(carrier, dest) %>% summarise(n()))
+# Use filter() on airlines to identify which airline corresponds to the carrier code.
+# Save the result from the filter on airlines to a variable, worst_delays.
+
+## Your code starts below this line
+
+worst_delays <- 
+
+## Your work ends before this line
